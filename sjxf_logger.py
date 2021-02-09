39c264d9119a737e1f928ad801ecced42d9f40bd
Nick <yaodongb@gmail.com>
2021-02-09 10:33:46 +0800

    添加存放数据库的接口，添加日志输出接口

---
+++ sjxf_logger.py
@@ -0,0 +1,46 @@
+# -*- coding=utf-8 -*-
+
+import os
+
+import logging
+from logging import handlers
+
+DEFAULT_LOG_FILE = "./log/sjxf.log"
+
+
+class SjxfLog:
+
+    def __init__(self, file_name=DEFAULT_LOG_FILE):
+
+        # 判断存放日志目录是否存在
+        if not os.path.exists(DEFAULT_LOG_FILE):
+            os.mkdir(os.path.dirname(DEFAULT_LOG_FILE))
+
+        self.__log = logging.getLogger()
+
+        # 设置日志文件以及各种格式
+        log_file_handler = logging.handlers.RotatingFileHandler(file_name)
+
+        log_format_str = "[%(asctime)s] [%(levelname)s] [%(filename)s] " \
+                         "[line:%(lineno)d] %(message)s"
+        date_format_str = "%Y-%m-%d %H:%M:%S"
+
+        log_format = logging.Formatter(fmt=log_format_str,
+                                       datefmt=date_format_str)
+        
+        log_file_handler.setFormatter(log_format)
+        
+        self.__log.addHandler(log_file_handler)
+
+        #
+        self.__log.setLevel(logging.DEBUG)
+
+
+    def debug(self, msg):
+        self.__log.debug(msg)
+
+    def error(self, msg):
+        self.__log.error(msg)
+
+    def info(self, msg):
+        self.__log.info(msg)
